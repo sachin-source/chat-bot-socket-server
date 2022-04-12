d7a73d28077f8c737c234bfe3bad912981803cb3
sachin-source <sachinmgvt@gmail.com>
2022-04-12 18:51:02 +0530

    handle two way communication with user and bot

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const redisHost = 'redis-16254.c264.ap-south-1-1.ec2.cloud.redislabs.com';
 const redisPort = 16254;
 const socketPORT = 8080;
 
-const allEventTypes = [ "supportMessage", "botMessage", "userMessage", "broadcast", "peerToPeer", "endPoint"];
+const allEventTypes = ["supportMessage", "botMessage", "userMessage", "broadcast", "peerToPeer", "endPoint"];
 
 /**
  * environment needs to be handled for databases.
@@ -19,41 +19,47 @@ const allEventTypes = [ "supportMessage", "botMessage", "userMessage", "broadcas
  */
 
 //  redis section
-const client = redis.createClient({
-  url: (environment == "development") ? '' :  ('redis://:' + redisPassword + '@' + (redisHost + ':' + redisPort))
+const redisClient = redis.createClient({
+  url: (environment == "development") ? '' : ('redis://:' + redisPassword + '@' + (redisHost + ':' + redisPort))
 });
 
 var messageQueue = null;
-const queueTopic = 'incomingMessages';
+var socketConnection = null;
+const incomingMessageQueue = 'incomingMessages';
+const savedMessageQueue = 'savedMessages';
 
-client.connect();
-amqp.connect('amqp://localhost', function(error0, connection) {
+redisClient.connect();
+amqp.connect('amqp://localhost', function (error0, connection) {
   if (error0) {
     throw error0;
   }
   console.log(error0 ? "connection error for rabbitmq" : "connection established for rabbitmq");
 
-  connection.createChannel(function(error1, channel) {
+  connection.createChannel(function (error1, incomingMessageChannel) {
     if (error1) {
       throw error1;
     }
-    var queue = 'hello';
-    var msg = 'Hello world';
 
-    channel.assertQueue(queueTopic, {
-      durable: false
-    });
+    incomingMessageChannel.assertQueue(incomingMessageQueue, { durable: false });
 
-    messageQueue = channel;
+    messageQueue = incomingMessageChannel;
     // channel.sendToQueue(queue, Buffer.from(msg));
     // console.log(" [x] Sent %s", msg);
   });
+
+  connection.createChannel(function (error1, savedMessageChannel) {
+    if (error1) {
+      throw error1;
+    }
+    savedMessageChannel.assertQueue(savedMessageQueue, { durable: false });
+    savedMessageChannel.consume(savedMessageQueue, sendMessageFromQueue, { noAck: true });
+  })
 });
 
-client.on('connect', () => {
+redisClient.on('connect', () => {
   console.log('Connected to redis');
 });
-client.on('error', (err) => console.log('Redis Client Error', err));
+redisClient.on('error', (err) => console.log('Redis client Error', err));
 
 /**
  * 
@@ -75,29 +81,80 @@ const io = new socketServer(socketPORT, {});
  * supportMessage and botMessage needs to authenticated also with loginToken and botSecret respectively
  */
 
+// const authenticate = (socket, next) => {
+//   const { token, userId, userType } = socket.handshake.query;
+//   (token && userId && (userType == "userMessage")) ? jwt.verify(token, AUTH_SECRET, function (err, decoded) {
+//     Boolean(err) ? next(new Error("not authorized")) : next();
+//   }) : next(new Error("not authorized"));
+// }
+
 const authenticate = (socket, next) => {
   const { token, userId, userType } = socket.handshake.query;
-  ( token && userId && (userType == "userMessage") ) ? jwt.verify(token, AUTH_SECRET, function (err, decoded) {
+  jwt.verify(token, AUTH_SECRET, function (err, decoded) {
     Boolean(err) ? next(new Error("not authorized")) : next();
-  }) : next(new Error("not authorized"));
+  });
 }
 
 io.use(authenticate);
 
 io.on("connection", (socket) => {
-  client.set(socket.handshake.query.userId, socket.id)
-  console.log("someone connected");
-
-  const regularEventTypes = [ "supportMessage", "botMessage", "userMessage" ];
-  regularEventTypes.forEach(event =>{
+  switch (socket.handshake.query.userType) {
+    case "userMessage":
+      redisClient.set(socket.handshake.query.userId, socket.id);
+      break;
+    case "supportMessage":
+      redisClient.set(socket.handshake.query.channelId + "Support", socket.id)
+      break;
+      case "botMessage":
+      // console.log(socket.id);
+      redisClient.set((socket.handshake.query.channelId + "Bot"), socket.id)
+      break;
+    default:
+      break;
+  }
+  // console.log("someone connected");
+  socketConnection = socket;
+  const regularEventTypes = ["supportMessage", "botMessage", "userMessage"];
+  regularEventTypes.forEach(event => {
     socket.on(event, (message) => {
       sendMessageToQueue(event, message);
     })
   })
+  // socket.on("supportMessage", (message) => {
+  //   sendMessageToQueue("supportMessage", message);
+  // })
+  // socket.on("botMessage", (message) => {
+  //   sendMessageToQueue("botMessage", message);
+  // })
+  // socket.on("userMessage", (message) => {
+  //   sendMessageToQueue("userMessage", message);
+  // })
 });
 
 const sendMessageToQueue = (event, message) => {
-  const messageToQueue = JSON.stringify(Object.assign({ messageType : event }, message))
-  messageQueue.sendToQueue(queueTopic, Buffer.from(messageToQueue));
-  console.log("Message sent to " + queueTopic + " Queue");
+  const messageToQueue = JSON.stringify(Object.assign({ messageType: event }, message))
+  messageQueue.sendToQueue(incomingMessageQueue, Buffer.from(messageToQueue));
+}
+
+const sendMessageFromQueue = (message) => {
+  const savedData = JSON.parse(message.content.toString());
+  switch (savedData.messageType) {
+    case "userMessage":
+      sendMessageToClient(savedData.channelId + "Support", savedData);
+      sendMessageToClient(savedData.channelId + "Bot", savedData);
+      break;
+    case "botMessage":
+      sendMessageToClient(savedData.userId, savedData);
+      break;
+    case "supportMessage":
+      sendMessageToClient(savedData.userId, savedData);
+      break;
+    default:
+      break;
+  }
+}
+
+const sendMessageToClient = async (target, data) => {
+  const clientTopic = await redisClient.get(target);
+  io.to(clientTopic).emit('message', JSON.stringify(data))
 }
